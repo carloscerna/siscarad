7925cb1e298cbc9c06da454676632004aef6546c
carloscerna <carlos_wilfredo_cerna@hotmail.com>
2022-06-01 15:04:25 -0600

    update-

--- resources/views/CalificacionPorAsignatura/index.blade.php
+++ resources/views/CalificacionPorAsignatura/index.blade.php
@@ -1,11 +1,28 @@
 @extends('layouts.app')
-
+/* llamada de valores
+@php
+use Illuminate\Support\Facades;
+    $correo_docente = Auth::user()->email;                                        
+    $nombre_docente = Auth::user()->name;
+    $codigo_personal = Auth::user()->codigo_personal;                                        
+@endphp
 @section('content')
-<x-slot name="header">
-  <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      {{ __('Select2') }}
-  </h2>
-</x-slot>
+@role("Docente")
+<section class="section">
+    <div class="section-header mb-1">
+        <h4 class="page__heading">{{$nombre_docente}} - {{$codigo_personal}}</h4>
+    </div>
+    <div class="section-body">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endrole
 
 <div class="form-group">
   {{ Form::label('LblAnnLectivo', 'Año Lectivo:') }}
